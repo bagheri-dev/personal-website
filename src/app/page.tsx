0e416baaa3d74ebcbb0294f3c7e44ab23dcd492f
mr-robot <bagheri.dev@gmail.com>
2025-05-22 11:30:32 +0330

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 import { AboutMe } from "@/Components/index/AboutMe";
-import { CodingActivity } from "@/Components/index/CodingActivity";
+// import { CodingActivity } from "@/Components/index/CodingActivity";
 import { Faqs } from "@/Components/index/Faqs";
 import { Hero } from "@/Components/index/Hero";
 import { Portfolio } from "@/Components/index/Portfolio";
-import { Socials } from "@/Components/index/Socials";
-import { Work } from "@/Components/index/Work";
+import {ContactMe} from "@/Components/index/Socials";
+import {CompetitiveAdvantages} from "@/Components/index/CompetitiveAdvantages";
+// import { Work } from "@/Components/index/Work";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutMe/>
-      <Work/>
+      {/*<Work/>*/}
       <Portfolio/>
-      <CodingActivity/>
+        <CompetitiveAdvantages />
+      {/*<CodingActivity/>*/}
       <Faqs />
-      <Socials/>
+      <ContactMe/>
     </>
   );
 }
